63267e6e1234d80d439f9fbcdd046a2aa02c1e26
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-07-03 20:02:20 +0900

    initial: Set up Vitacheck project

---
+++ src/main/java/com/vitacheck/VitacheckBeApplication.java
@@ -0,0 +1,13 @@
+package com.vitacheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitacheckBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitacheckBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vitacheck/VitacheckBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitacheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitacheckBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
