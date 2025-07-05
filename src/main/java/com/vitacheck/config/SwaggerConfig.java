13ce02b9a8cdaace4e668a5796756961567758cd
m2 <orum1223@dgu.ac.kr>
2025-07-05 19:25:30 +0900

    Revert "feat(#9):스웨거 추가"
    This reverts commit 9a979c1ef671e8b07e25cd3e692d4c7c205e73d7.

--- src/main/java/com/vitacheck/config/SwaggerConfig.java
+++ src/main/java/com/vitacheck/config/SwaggerConfig.java
@@ -1,38 +1,4 @@
 package com.vitacheck.config;
 
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI openAPI() {
-        // API 문서에 대한 기본 정보 설정
-        Info info = new Info()
-                .title("\uD83D\uDC8AVitaCheck API")
-                .version("1.0.0")
-                .description("비타체크 API 명세서입니다.");
-
-        // JWT 인증 스키마 설정
-        String jwtSchemeName = "JWT Authentication";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
-        Components components = new Components()
-                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
-                        .name(jwtSchemeName)
-                        .type(SecurityScheme.Type.HTTP) // HTTP 타입
-                        .scheme("bearer") // Bearer 토큰 방식 사용
-                        .bearerFormat("JWT")); // 토큰 형식 지정
-
-        return new OpenAPI()
-                .info(info)
-                .addSecurityItem(securityRequirement)
-                .components(components);
-    }
 }
