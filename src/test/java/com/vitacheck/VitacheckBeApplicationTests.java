531a08a3b5d513102078d6d238de75f2675d7bb8
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-07-18 01:02:40 +0900

    fix: ci/cd

--- src/test/java/com/vitacheck/VitacheckBeApplicationTests.java
+++ src/test/java/com/vitacheck/VitacheckBeApplicationTests.java
@@ -1,12 +1,21 @@
 package com.vitacheck;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class VitacheckBeApplicationTests {
+
+    @MockBean
+    private FirebaseApp firebaseApp;
+
+    @MockBean
+    private FirebaseMessaging firebaseMessaging;
 
     @Test
     void contextLoads() {
