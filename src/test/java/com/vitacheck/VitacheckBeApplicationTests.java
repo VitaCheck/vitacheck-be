7182fa8fed03b2d2d71aa11db052c92226caa356
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-07-19 16:04:44 +0900

    fix: test용 application 파일 수정

--- src/test/java/com/vitacheck/VitacheckBeApplicationTests.java
+++ src/test/java/com/vitacheck/VitacheckBeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.vitacheck;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,9 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class VitacheckBeApplicationTests {
+
+    @MockBean
+    private AmazonS3 amazonS3;
 
     @Test
     void contextLoads() {
