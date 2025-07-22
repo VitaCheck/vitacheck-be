0b894bf0c303779f2a2f86c727df2dafea166413
m2 <orum1223@dgu.ac.kr>
2025-07-22 14:39:21 +0900

    fix(#97):스웨거 인증 오류 수정

--- src/main/java/com/vitacheck/repository/SupplementRepository.java
+++ src/main/java/com/vitacheck/repository/SupplementRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SupplementRepository extends JpaRepository<Supplement, Long>, SupplementRepositoryCustom {
-
     @Query("SELECT s FROM Supplement s JOIN FETCH s.supplementIngredients si JOIN FETCH si.ingredient WHERE s.id IN :ids")
     List<Supplement> findSupplementsWithIngredientsByIds(@Param("ids") List<Long> ids);
 }
