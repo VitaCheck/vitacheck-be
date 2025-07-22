2290625881e0bae5a747da4221819fa48995d874
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-07-22 22:34:31 +0900

    fix(#104): UserDto 중복 클래스 문제 해결

--- src/main/java/com/vitacheck/dto/UserDto.java
+++ src/main/java/com/vitacheck/dto/UserDto.java
@@ -98,10 +98,4 @@ public class UserDto {
     public static class UpdateFcmTokenRequest {
         private String fcmToken;
     }
-
-    @Getter
-    @NoArgsConstructor
-    public static class UpdateFcmTokenRequest {
-        private String fcmToken;
-    }
 }
