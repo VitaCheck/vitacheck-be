d20217ef1dbb790370e99dfac5727df10e8acff7
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-07-24 19:48:10 +0900

    fix(#108): cors 설정 변경

--- src/main/java/com/vitacheck/config/SecurityConfig.java
+++ src/main/java/com/vitacheck/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // 프론트엔드 개발 서버 주소 허용
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
 
         // 허용할 HTTP 메서드
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
