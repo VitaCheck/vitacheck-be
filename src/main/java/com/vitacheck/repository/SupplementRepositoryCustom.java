60578aee01d77aea9a45fdd068cc2ab9e3405a4a
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-08-15 10:06:00 +0900

    fix: queryDsl 수정

--- src/main/java/com/vitacheck/repository/SupplementRepositoryCustom.java
+++ src/main/java/com/vitacheck/repository/SupplementRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.vitacheck.repository;
 
 import com.vitacheck.domain.Supplement;
+import com.vitacheck.domain.purposes.AllPurpose;
 import com.vitacheck.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,4 +10,5 @@ import java.util.List;
 
 public interface SupplementRepositoryCustom {
     Page<Supplement> search(User user, String keyword, String brandName, String ingredientName, Pageable pageable);
+    Page<Supplement> findByPurposeNames(List<AllPurpose> purposeNames, Pageable pageable);
 }
