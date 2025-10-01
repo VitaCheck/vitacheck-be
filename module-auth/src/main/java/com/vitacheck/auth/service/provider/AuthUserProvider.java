a86b06b6eeab6198aab542d4431fa332d3a2f2bb
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-10-01 09:55:14 +0900

    fix(#354): 회원가입 데이터 처리 user 모듈에서 진행하도록 변경

--- module-auth/src/main/java/com/vitacheck/auth/service/provider/AuthUserProvider.java
+++
@@ -1,9 +0,0 @@
-package com.vitacheck.auth.service.provider;
-
-import com.vitacheck.auth.dto.AuthUserDto;
-
-import java.util.Optional;
-
-public interface AuthUserProvider {
-    Optional<AuthUserDto> findByEmail(String email);
-}
--- module-auth/src/main/java/com/vitacheck/auth/service/provider/UserRegistrationService.java
+++
@@ -1,7 +0,0 @@
-package com.vitacheck.auth.service.provider;
-
-import com.vitacheck.auth.dto.CreateUserRequest;
-
-public interface UserRegistrationService {
-    void registerUser(CreateUserRequest request);
-}
