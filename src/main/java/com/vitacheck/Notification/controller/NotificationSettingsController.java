256df9b94350a95bf71526810eb4c306162e4936
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-10-29 12:47:17 +0900

    hotfix: ip 확인 작업 주석처리

--- src/main/java/com/vitacheck/Notification/controller/NotificationSettingsController.java
+++ src/main/java/com/vitacheck/Notification/controller/NotificationSettingsController.java
@@ -93,11 +93,11 @@ public class NotificationSettingsController {
     @Operation(summary = "내부용 알림 발송 트리거", description = "Cron Job에 의해 호출되어 알림 발송 로직을 실행합니다. (외부 노출 금지)")
     public CustomResponse<String> triggerNotifications(HttpServletRequest request) {
         // 보안을 위해 localhost에서 온 요청인지 확인합니다.
-        String remoteAddr = request.getRemoteAddr();
-        if (!remoteAddr.equals("127.0.0.1") && !remoteAddr.equals("0:0:0:0:0:0:0:1")) {
-            // 권한 없음 에러를 반환합니다.
-            throw new CustomException(ErrorCode.INVALID_REQUEST);
-        }
+//        String remoteAddr = request.getRemoteAddr();
+//        if (!remoteAddr.equals("127.0.0.1") && !remoteAddr.equals("0:0:0:0:0:0:0:1")) {
+//            // 권한 없음 에러를 반환합니다.
+//            throw new CustomException(ErrorCode.INVALID_REQUEST);
+//        }
 
         notificationScheduler.sendRoutineNotifications();
         return CustomResponse.ok("알림 발송 작업이 성공적으로 실행되었습니다.");
